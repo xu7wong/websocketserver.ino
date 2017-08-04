34e0900e95f0498c8164f28d9fd9790ff4ce9673
Huang-PC\Huang <xu@3dprintingsystems.com>
2017-08-04 20:49:11 +1200

    change AP name

--- websocketserver.ino.ino
+++ websocketserver.ino.ino
@@ -31,169 +31,170 @@ bool socketConnected = false;
 #define SEND_SERIAL_TIME (50)
 
 class SerialTerminal {
-    public:
-        void setup() {
-            _lastRX = 0;
-            resetBuffer();
-            Serial.begin(115200);
+  public:
+    void setup() {
+      _lastRX = 0;
+      resetBuffer();
+      Serial.begin(115200);
+    }
+
+    void loop() {
+      unsigned long t = millis();
+      bool forceSend = false;
+
+      size_t len = (_bufferWritePtr - &_buffer[0]);
+      int free = (sizeof(_buffer) - len);
+
+      int available = Serial.available();
+      if (available > 0 && free > 0) {
+        int readBytes = available;
+        if (readBytes > free) {
+          readBytes = free;
         }
+        readBytes = Serial.readBytes(_bufferWritePtr, readBytes);
+        _bufferWritePtr += readBytes;
+        _lastRX = t;
+      }
 
-        void loop() {
-            unsigned long t = millis();
-            bool forceSend = false;
-
-            size_t len = (_bufferWritePtr - &_buffer[0]);
-            int free = (sizeof(_buffer) - len);
-
-            int available = Serial.available();
-            if(available > 0 && free > 0) {
-                int readBytes = available;
-                if(readBytes > free) {
-                    readBytes = free;
-                }
-                readBytes = Serial.readBytes(_bufferWritePtr, readBytes);
-                _bufferWritePtr += readBytes;
-                _lastRX = t;
-            }
-
-            // check for data in buffer
-            len = (_bufferWritePtr - &_buffer[0]);
-            if(len >=  sizeof(_buffer)) {
-                forceSend = true;
-            }
-            if(len > (WEBSOCKETS_MAX_HEADER_SIZE + 1)) {
-                if(((t - _lastRX) > SEND_SERIAL_TIME) || forceSend) {
-                    //Serial1.printf("broadcastBIN forceSend: %d\n", forceSend);
-                    webSocket.broadcastTXT(&_buffer[0], (len - WEBSOCKETS_MAX_HEADER_SIZE), true);
-                    resetBuffer();
-                }
-            }
+      // check for data in buffer
+      len = (_bufferWritePtr - &_buffer[0]);
+      if (len >=  sizeof(_buffer)) {
+        forceSend = true;
+      }
+      if (len > (WEBSOCKETS_MAX_HEADER_SIZE + 1)) {
+        if (((t - _lastRX) > SEND_SERIAL_TIME) || forceSend) {
+          //Serial1.printf("broadcastBIN forceSend: %d\n", forceSend);
+          webSocket.broadcastTXT(&_buffer[0], (len - WEBSOCKETS_MAX_HEADER_SIZE), true);
+          resetBuffer();
         }
+      }
+    }
 
 
-    protected:
-        uint8_t _buffer[1460];
-        uint8_t * _bufferWritePtr;
-        unsigned long _lastRX;
+  protected:
+    uint8_t _buffer[1460];
+    uint8_t * _bufferWritePtr;
+    unsigned long _lastRX;
 
-        void resetBuffer() {
-            // offset for adding Websocket header
-            _bufferWritePtr = &_buffer[WEBSOCKETS_MAX_HEADER_SIZE];
-            // addChar('T');
-        }
+    void resetBuffer() {
+      // offset for adding Websocket header
+      _bufferWritePtr = &_buffer[WEBSOCKETS_MAX_HEADER_SIZE];
+      // addChar('T');
+    }
 
-        inline void addChar(char c) {
-            *_bufferWritePtr = (uint8_t) c; // message type for Webinterface
-            _bufferWritePtr++;
-        }
+    inline void addChar(char c) {
+      *_bufferWritePtr = (uint8_t) c; // message type for Webinterface
+      _bufferWritePtr++;
+    }
 };
 
 SerialTerminal term;
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
-    switch(type) {
-        case WStype_DISCONNECTED:
-            socketConnected = false;
-//            Serial1.printf("[%u] Disconnected!\n", num);
-            break;
-        case WStype_CONNECTED: {
-            IPAddress ip = webSocket.remoteIP(num);
-            socketConnected = true;
-//            Serial1.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-            // send message to client
-            webSocket.sendTXT(num, "Connected");
+  switch (type) {
+    case WStype_DISCONNECTED:
+      socketConnected = false;
+      //            Serial1.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {
+        IPAddress ip = webSocket.remoteIP(num);
+        socketConnected = true;
+        //            Serial1.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
+      }
+      break;
+    case WStype_TEXT:
+      //            Serial1.printf("[%u] get Text: %s\n", num, payload);
+      if (lenght > 0) {
+        String command = String((const char *)payload);
+        if (command.indexOf("resetWiFi") != -1) {
+          webSocket.sendTXT(num, "Resetting WiFi settings...");
+          delay(500);
+          //wifiManager.resetSettings();
+          delay(100);
+          //ESP.restart();
         }
-            break;
-        case WStype_TEXT:
-//            Serial1.printf("[%u] get Text: %s\n", num, payload);
-            if(lenght > 0) {
-                String command = String((const char *)payload);
-                if (command.indexOf("resetWiFi") != -1){
-                    webSocket.sendTXT(num, "Resetting WiFi settings...");
-                    delay(500);
-                    //wifiManager.resetSettings();
-                    delay(100);
-                    //ESP.restart();
-                }
-                Serial.write((const char *) (payload), (lenght));
-            }
-            break;
-    }
+        Serial.write((const char *) (payload), (lenght));
+      }
+      break;
+  }
 }
 
 void setup()
 {
   WiFi.mode(WIFI_AP_STA);//AP and client mode
-  
-  WiFi.enableAP(false);//disable AP mode, 
-  WiFi.enableSTA(true);//enable client mode, 
-  
-    // use Serial 1 for debug out
-//    Serial1.begin(921600);
-//    Serial1.setDebugOutput(true);
 
-//    Serial1.println();
-//    Serial1.println();
-//    Serial1.println();
+  WiFi.enableAP(false);//disable AP mode,
+  WiFi.enableSTA(true);//enable client mode,
 
-    for(uint8_t t = 4; t > 0; t--) {
-//        Serial1.printf("[SETUP] BOOT WAIT %d...\n", t);
-        delay(1000);
-    }
+  // use Serial 1 for debug out
+  //    Serial1.begin(921600);
+  //    Serial1.setDebugOutput(true);
 
-//    Serial1.printf("[SETUP] HEAP: %d\n", ESP.getFreeHeap());
+  //    Serial1.println();
+  //    Serial1.println();
+  //    Serial1.println();
 
-    pinMode(RESET_PIN, INPUT_PULLUP);
+  for (uint8_t t = 4; t > 0; t--) {
+    //        Serial1.printf("[SETUP] BOOT WAIT %d...\n", t);
+    delay(1000);
+  }
 
-    //WiFiManager wifiManager;
-    //wifiManager.resetSettings();    
-        
-    wifiManager.autoConnect("Emblaser2");
-  
+  //    Serial1.printf("[SETUP] HEAP: %d\n", ESP.getFreeHeap());
+
+  pinMode(RESET_PIN, INPUT_PULLUP);
+
+  //WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+
+  wifiManager.autoConnect("Emblaser2");
+
   IPAddress ip_addr = WiFi.localIP();
-  String("Emblaser2 IP:"+String(ip_addr[0])+"."+String(ip_addr[1])+"."+String(ip_addr[2])+"."+String(ip_addr[3])).toCharArray(AP_ssid, 32);
+  String("E2_" + WiFi.macAddress()).toCharArray(AP_ssid, 32);
+  //String("Emblaser2 IP:"+String(ip_addr[0])+"."+String(ip_addr[1])+"."+String(ip_addr[2])+"."+String(ip_addr[3])).toCharArray(AP_ssid, 32);
   WiFi.enableAP(true);//enable AP to broadcast IP address to users, can be turn off again if necessary
   delay(500);
   WiFi.softAP(AP_ssid, "12345678");
-  
-    webSocket.begin();
-    webSocket.onEvent(webSocketEvent);
 
-    httpUpdater.setup(&httpServer, update_path, update_username, update_password);
-    httpServer.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 
-    term.setup();
+  httpUpdater.setup(&httpServer, update_path, update_username, update_password);
+  httpServer.begin();
 
-    // disable WiFi sleep for more performance
-    WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  term.setup();
+
+  // disable WiFi sleep for more performance
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
 }
 
 
 void loop()
 {
-    term.loop();
-    webSocket.loop();
-    httpServer.handleClient();
+  term.loop();
+  webSocket.loop();
+  httpServer.handleClient();
 
-    if (socketConnected == false){
-        //check every 10 loops
-        if (loopCount > 100){
-            loopCount = 0;
-            // if GPIO0 is LOW for 10 times -> reset settings
-            if (resetCount > 10){
-                resetCount = 0;
-                //wifiManager.resetSettings();
-                delay(100);   
-                //ESP.restart();
-            } else {
-                if (digitalRead(RESET_PIN) == LOW) {
-                    resetCount++;
-                } else {
-                    resetCount = 0;
-                }
-            }
+  if (socketConnected == false) {
+    //check every 10 loops
+    if (loopCount > 100) {
+      loopCount = 0;
+      // if GPIO0 is LOW for 10 times -> reset settings
+      if (resetCount > 10) {
+        resetCount = 0;
+        //wifiManager.resetSettings();
+        delay(100);
+        //ESP.restart();
+      } else {
+        if (digitalRead(RESET_PIN) == LOW) {
+          resetCount++;
         } else {
-            loopCount++;
+          resetCount = 0;
         }
+      }
+    } else {
+      loopCount++;
     }
+  }
 }
